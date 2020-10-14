ebc0f1cadb5e8ef6b6320b1ac97db438c2a86ec6
Pagwin <Pagwin.Fedora@protonmail.com>
2020-10-14 18:52:02 -0400

    improved

--- generator.py
+++ generator.py
@@ -1,13 +1,37 @@
-print("print('what operator do you want to do?');\nops=['+','-','x','/']\noperator=None\nwhile not operator in ops:\n\toperator = input('+ - x /\\t')\nx=-1\nwhile not 0<=x<=199:\n\tx=int(input('what\\'s the first num you want to input?(0-200)'))\ny=-1\nwhile not 0<=y<=199:\n\ty=int(input('what\\'s the second num you want to input?(0-200)'))")
-for o in ["+","-","x","/"]:
-    print("if operator == '"+o+"':")
-    for i in range(200):
-        for n in range(200):
-            s = """
-    if x == {0} and y == {1}:
-        print('{0}{3}{1}={2}')
-        """
-            if o == "/" and n == 0:
-                continue
-            s = s.format(i,n,eval("i{0}n".format(o.replace("x","*"))),o)
-            print(s)
+import itertools
+
+MAX = 200
+
+single = """
+if x == {0} and y == {1} and operator == '{2}':
+    print("{0}{2}{1} = {3}")
+"""
+
+combs = itertools.product(range(MAX), range(MAX), "+-x/")
+
+
+def handler(tup):
+    first, second, operator = tup
+    if operator == "/" and second == 0:
+        print(
+            """
+if x == {0} and y == {1} and operator == '{2}':
+    print("{0}{2}{1} = undefined")""".format(
+                first, second, operator.replace("x", "*")
+            )
+        )
+    else:
+        print(
+            single.format(
+                first,
+                second,
+                operator,
+                eval(f"{first}{operator.replace('x','*')}{second}"),
+            )
+        )
+
+
+print(
+    "print('what operator do you want to do?');\nops=['+','-','x','/']\noperator=None\nwhile not operator in ops:\n\toperator = input('+ - x /\\t')\nx=-1\nwhile not 0<=x<=199:\n\tx=int(input('what\\'s the first num you want to input?(0-200)'))\ny=-1\nwhile not 0<=y<=199:\n\ty=int(input('what\\'s the second num you want to input?(0-200)'))"
+)
+[*map(handler, combs)]
