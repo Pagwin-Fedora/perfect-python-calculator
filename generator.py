3207c680ecdac2ace4fa11c5a861b84f64b2b2e6
Pagwin <git@pagwin.xyz>
2021-11-08 15:08:45 -0500

    modified the generator to work in a dataset generating context if that's needed for some reason

--- generator.py
+++ generator.py
@@ -2,36 +2,21 @@ import itertools
 
 MAX = 200
 
-single = """
-if x == {0} and y == {1} and operator == '{2}':
-    print("{0}{2}{1} = {3}")
-"""
+single = """{0}{2}{1}"""
 
-combs = itertools.product(range(MAX), range(MAX), "+-x/")
+combs = itertools.product(range(MAX), range(MAX), list("+-*/")+["//"])
 
 
 def handler(tup):
     first, second, operator = tup
-    if operator == "/" and second == 0:
-        print(
-            """
-if x == {0} and y == {1} and operator == '{2}':
-    print("{0}{2}{1} = undefined")""".format(
-                first, second, operator.replace("x", "*")
-            )
-        )
-    else:
-        print(
-            single.format(
-                first,
-                second,
-                operator,
-                eval(f"{first}{operator.replace('x','*')}{second}"),
-            )
-        )
+    with open("question","a+") as question:
+        with open("answer","a+") as answer:
+            if (operator == "/" or operator == "//" ) and second == 0:
+                print(single.format(first,second,operator),file=question)
+                print("stop fudging with maths",file=answer)
+            else:
+                print(single.format(first,second,operator),file=question)
+                print(eval(f"{first}{operator}{second}"),file=answer)
 
 
-print(
-    "print('what operator do you want to do?');\nops='+-x/'\noperator=None\nwhile not operator in ops:\n\toperator = input('+ - x /\\t')\nx=-1\nwhile not 0<=x<=199:\n\tx=int(input('what\\'s the first num you want to input?(0-200)'))\ny=-1\nwhile not 0<=y<=199:\n\ty=int(input('what\\'s the second num you want to input?(0-200)'))"
-)
 [*map(handler, combs)]
